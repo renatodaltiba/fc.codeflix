c44ca678f6520e2925d04956d691aa86a6963bfe
Renato D. Schneider <renatodaltiba@gmail.com>
2023-07-04 21:49:42 -0300

    feat: added domain validation

--- src/FC.Codeflix.Catalog.Domain/Validation/DomainValidation.cs
+++ src/FC.Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -9,4 +9,22 @@ public class DomainValidation
         if (target is null)
             throw new EntityValidationException($"{fieldName} should not be null");
     }
+
+    public static void NotNullOrEmpty(string? target, string fieldName)
+    {
+        if (string.IsNullOrWhiteSpace(target))
+            throw new EntityValidationException($"{fieldName} should not be null or empty");
+    }
+    
+    public static void MinLength(string? target, string fieldName, int minLength)
+    {
+        if (target?.Length < minLength)
+            throw new EntityValidationException($"{fieldName} should have at least {minLength} characters");
+    }
+    
+    public static void MaxLength(string? target, string fieldName, int maxLength)
+    {
+        if (target?.Length > maxLength)
+            throw new EntityValidationException($"{fieldName} should have at most {maxLength} characters");
+    }
 }
